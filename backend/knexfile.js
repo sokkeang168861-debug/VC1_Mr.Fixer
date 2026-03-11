575f59a7571f8ee972063dfb1d37edf647e15db7
Chhanut-Source <chhanuth21@gmail.com>
2026-03-11 19:08:28 +0700

    .env .env.example

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -9,7 +10,7 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 3307,
+      port: parseInt(process.env.DB_PORT, 10) || 3307,
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '',
       database: process.env.DB_NAME || 'mr_fixer_db',
